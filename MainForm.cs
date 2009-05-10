3c65436ab020dee660d5be72684bd92f4870d723
bitstest <bitstest@537e259d-f3cc-4ea6-b84d-474af18c052d>
2009-05-10 20:49:30 +0000

    Committing first version of the code. (Works).

---
+++ MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace BACnetInteropApp
+{
+    public partial class MainForm : Form
+    {
+
+        static void SendWhoIs()
+        {
+            byte[] data = new byte[1024];
+            int optr = 0;
+
+            //            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.0.103"), 0xBAC0 );
+            //            IPEndPoint local_ipep = new IPEndPoint(0, 0xBAC0);
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Broadcast, 0xBAC0);
+
+            IPEndPoint local_ipep = new IPEndPoint(0, 0xBAC1);// NOTE, NOT BAC0, so that we can run IUT bacnet server on same machine
+
+            Socket bacnet_master_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // bind the local end of the connection to BACnet port number
+            bacnet_master_socket.Bind(local_ipep);
+
+            bacnet_master_socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+
+            data[optr++] = BACnetEnums.BACNET_BVLC_TYPE_BIP ;
+            data[optr++] = BACnetEnums.BACNET_ORIGINAL_BROADCAST_NPDU;
+            data[optr++] = 0x00;
+            data[optr++] = 0x0c;
+            data[optr++] = 0x01;
+            data[optr++] = 0x20;
+            data[optr++] = 0xff;
+            data[optr++] = 0xff;
+            data[optr++] = 0x00;
+            data[optr++] = 0xff;
+            data[optr++] = 0x10;
+            data[optr++] = 0x08;
+
+            Console.WriteLine("Sending\n");
+
+            bacnet_master_socket.SendTo(data, optr, SocketFlags.None, ipep);
+
+            //IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            //EndPoint tmpRemote = (EndPoint)sender;
+
+            //data = new byte[1024];
+            //int recv = bacnet_master_socket.ReceiveFrom(data, ref tmpRemote);
+
+            //Console.WriteLine("Message received from {0}:", tmpRemote.ToString());
+            //Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            //Console.WriteLine(Encoding.ASCII.GetBytes((char[])data, 0, recv));
+            /*
+                        server.SendTo(Encoding.ASCII.GetBytes("message 1"), tmpRemote);
+                        server.SendTo(Encoding.ASCII.GetBytes("message 2"), tmpRemote);
+                        server.SendTo(Encoding.ASCII.GetBytes("message 3"), tmpRemote);
+                        server.SendTo(Encoding.ASCII.GetBytes("message 4"), tmpRemote);
+                        server.SendTo(Encoding.ASCII.GetBytes("message 5"), tmpRemote);
+            */
+
+            // Console.WriteLine("Stopping client");
+            bacnet_master_socket.Close();
+
+        }
+
+        Alpha oAlpha;
+        Thread oThread;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // fire up a thread to watch for incoming packets
+
+            oAlpha = new Alpha();
+            
+            oThread = new Thread(new ThreadStart(oAlpha.Beta));
+            oThread.Start();
+
+        }
+
+
+
+        private void ExpandAllButton_Click(object sender, EventArgs e)
+        {
+            this.treeView2.ExpandAll();     
+
+
+        }
+
+        private void CollapseAllButton_Click(object sender, EventArgs e)
+        {
+            this.treeView2.CollapseAll();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TreeNode mainNode = new TreeNode();
+
+            mainNode.Name = "mainNode";
+
+            mainNode.Text = "Added";
+
+            this.treeView2.Nodes.Add(mainNode);
+        }
+
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNode nod = new TreeNode();
+            nod.Name = "able bunny";
+            nod.Text = "able bunny2";
+            nod.Tag = "able bunny3";
+            this.treeView2.SelectedNode.Nodes.Add(nod);
+            this.treeView2.SelectedNode.ExpandAll();
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TreeNode nod = new TreeNode();
+            nod.Name = "able bunny";
+            nod.Text = "able bunny2";
+            nod.Tag = "able bunny3";
+            this.treeView2.SelectedNode.Nodes.Add(nod);
+            this.treeView2.SelectedNode.Expand();
+        }
+
+        static int snode = 0;
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.treeView2.SelectedNode = this.treeView2.Nodes[snode++];
+            this.treeView2.SelectedNode.ExpandAll();
+        }
+
+        private void helpToolStripButton_Click(object sender, EventArgs e)
+        {
+            AboutBox1 mab = new AboutBox1();
+
+            mab.ShowDialog();
+        }
+
+        //private void toolStripButton1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void SendWhoIsButton(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Sending Who_is");
+            SendWhoIs();
+        }
+
+
+
+        List<Device> ourdevices = new List<Device>();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Check to see if there is a new "I-Am" message to process.
+
+            while (  oAlpha.NewDeviceQueue.Count != 0 ) {
+
+                Device D = oAlpha.NewDeviceQueue.Dequeue();
+
+                // is this a new Device?
+
+                if (!ourdevices.Contains(D))
+                {
+                    // is new
+
+                    TreeNode NewNode = new TreeNode();
+
+                    NewNode.Name = "NewNode";
+
+                    NewNode.Text = "Device " + D.DeviceId ;
+                    NewNode.Tag = D.DeviceId ;
+
+                    this.treeView2.Nodes.Add(NewNode);
+                    this.treeView2.SelectedNode = NewNode;
+
+                    // add other paramters to our new node
+
+                    this.treeView2.SelectedNode.Nodes.Add( "Vendor ID      " + D.VendorId );
+                    this.treeView2.SelectedNode.Nodes.Add( "Network Number " + D.NetworkNumber );
+                    
+                    /*
+                    TreeNode ntn = new TreeNode( "Test4" ) ;
+
+                    this.treeView2.SelectedNode.Nodes.Add(ntn);
+
+                    TreeNode sn = this.treeView2.SelectedNode.Nodes[0];
+
+                    sn.Nodes.Add("Test5");
+                    */
+
+                    ourdevices.Add(D);
+                }
+                else
+                {
+                    //if we get here, we _know_ that treeview2 contains our node
+                                        
+                    for ( int i=0; i<this.treeView2.Nodes.Count; i++ )
+                    {
+                        TreeNode tni = this.treeView2.Nodes[i];
+
+                        if (tni.Tag != null)
+                        {
+                            if (tni.Tag.Equals(D.DeviceId))
+                            {
+                                // found it.. select it
+                                this.treeView2.SelectedNode = tni;
+                                
+                            }
+                        }
+                        
+                    }
+                }
+
+            }
+
+            }
+                       
+    
+        private void button7_Click(object sender, EventArgs e)
+        {
+            oThread.Abort();
+            //oThread.Join(2000);
+
+            // cancel our outstanding receive call
+            try
+            {
+                oAlpha.BetaClose();
+            }
+            catch( Exception fe )
+            {
+                Console.WriteLine(fe);
+            }
+
+            Application.Exit();
+        }
+
+        // on shutdown
+
+        
+
+    }
+}
---
+++ Parse.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BACnetInteropApp
+{
+
+    class Parse
+    {
+    }
+
+    public class Alpha
+    {
+        public int timekeeper;
+        public List<Device> Devicelist = new List<Device>();
+
+        public Queue<Device> NewDeviceQueue = new Queue<Device>();
+
+        public void parse(byte[] bytes)
+        {
+            byte[] temp;
+            int apdu_offset = 0;
+            int npdu_offset = 0;
+
+            // this whole section http://www.bacnetwiki.com/wiki/index.php?title=BACnet_Virtual_Link_Control
+
+            /*
+                        temp = new byte[2];
+                        temp[0] = bytes[7];
+                        temp[1] = bytes[6];
+                        int destNet = BitConverter.ToUInt16(temp, 0);
+                        Console.WriteLine("Destination network: " + destNet);
+
+                        Console.WriteLine("Dest. MAC layer address: " + bytes[8]);
+
+                        Console.WriteLine("Hop count: " + bytes[9]);
+
+            */
+
+            if (bytes[0] != BACnetEnums.BACNET_BVLC_TYPE_BIP)
+            {
+                // todo3, panic log
+                Console.WriteLine("Not a BACnet/IP message");
+                return;
+            }
+
+            // we could receive an original broadcast, a unicast, a forwarded here...
+            // right now
+
+            switch (bytes[1])
+            {
+                case BACnetEnums.BACNET_UNICAST_NPDU:
+                case BACnetEnums.BACNET_FORWARDED_NPDU:
+                    npdu_offset = 10;
+                    break;
+                case BACnetEnums.BACNET_ORIGINAL_BROADCAST_NPDU:
+                    // all these OK
+                    npdu_offset = 4;
+                    break;
+                default:
+                    // OK, do a panic here
+                    //todo1
+                    break;
+            }
+
+            int len = bytes[2] + bytes[3];
+
+            if (bytes[npdu_offset] != BACnetEnums.BACNET_PROTOCOL_VERSION)
+            {
+                // we have a major problem, microprotocol version has changed. http://www.bacnetwiki.com/wiki/index.php?title=BACnet_Virtual_Link_Control
+                // todo3, panic
+                Console.WriteLine("Protocol version problem");
+                return;
+            }
+
+
+            if (bytes[npdu_offset + 1] != 0x20)
+            {
+                // todo2, investigate
+                Console.WriteLine("Control flags not OK");
+            }
+
+            apdu_offset = npdu_offset + 6;
+
+            // the offset here is the APDU. Start parsing APDU.
+
+            if (bytes[apdu_offset] == (byte)BACnetEnums.BACNET_PDU_TYPE.PDU_TYPE_UNCONFIRMED_SERVICE_REQUEST)
+            {
+                Console.WriteLine("This is an unconfirmed request");
+            }
+
+            if (bytes[apdu_offset + 1] == (byte)BACnetEnums.BACNET_UNCONFIRMED_SERVICE.SERVICE_UNCONFIRMED_WHO_IS)
+            {
+                Console.WriteLine("Who-is");
+            }
+            else if (bytes[apdu_offset + 1] == (byte)BACnetEnums.BACNET_UNCONFIRMED_SERVICE.SERVICE_UNCONFIRMED_I_AM)
+            {
+                Console.WriteLine("We have received an 'i-Am'");
+
+                Device D = new Device();
+
+                //bytes[12];
+                //bytes[13];
+                //bytes[14];
+                //bytes[15];
+                temp = new byte[4];
+                temp[0] = bytes[apdu_offset + 6];
+                temp[1] = bytes[apdu_offset + 5];
+                temp[2] = bytes[apdu_offset + 4];
+                temp[3] = 0x00;
+                int deviceId = BitConverter.ToInt32(temp, 0);
+                Console.WriteLine("This is device: " + deviceId);
+
+                D.DeviceId = deviceId;
+
+
+                //bytes[17];
+                temp = new byte[2];
+                temp[0] = bytes[apdu_offset + 9];
+                temp[1] = bytes[apdu_offset + 8];
+                //int maxAPDULen = bytes[18];
+                int maxAPDULen = BitConverter.ToInt16(temp, 0);
+                Console.WriteLine("Max APDU length accepted: " + maxAPDULen);
+
+                //bytes[19];
+                //bytes[20];
+
+                // todo, we are not decoding an encoded value properly here
+                int vendorId = bytes[apdu_offset + 14] + bytes[apdu_offset + 13] * 256;
+                Console.WriteLine("VendorId: " + vendorId);
+
+                D.VendorId = vendorId;
+                D.NetworkNumber = 0;
+
+                // Devicelist.Add(D);
+                NewDeviceQueue.Enqueue(D);
+                
+            }
+
+        }
+
+
+
+
+        UdpClient udpRecClient;
+
+        public void BetaClose()
+        {
+            udpRecClient.Close();
+        }
+
+
+        // This method that will be called when the thread is started
+        public void Beta()
+        {
+            Console.WriteLine("Thread starting");
+
+            //            IPAddress mcAddress = IPAddress.Parse("224.0.0.1");
+            //            udpRecClient.JoinMulticastGroup(mcAddress);
+
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+
+            // Open the BACnet socket. There is a chance that another BACnet program is running on this machine. Anticipate this. // todo-allow this
+            try
+            {
+                udpRecClient = new UdpClient(0xBAC0, AddressFamily.InterNetwork);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+            }
+
+            while (true)
+            {
+                Byte[] received;
+
+                Console.Write(".");
+
+                try
+                {
+                    received = udpRecClient.Receive(ref remoteIpEndPoint);
+
+                    timekeeper++;
+
+                    //Console.WriteLine("dataReceived: " + received.Length + " bytes...");
+                    //Console.WriteLine("From: " + remoteIpEndPoint + "  Count " + timekeeper);
+
+                    parse(received);
+                }
+                catch (Exception efe)
+                {
+                    // need to catch the inevitable exception when this blocking call is cancelled by the shutdown code
+                    Console.WriteLine(efe);
+                }
+            }
+        }
+    }
+}
